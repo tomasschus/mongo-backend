f26921fb499ff475f91f4c7ee7e74c1caab40ea4
Tomas Schuster <ts2525@hotmail.com>
2023-05-25 17:02:23 -0300

    remove console log

--- src/helpers/isGivenNoteAfter.ts
+++ src/helpers/isGivenNoteAfter.ts
@@ -4,8 +4,6 @@ import { Note } from "src/note/schemas/notes.schema";
 export function isGivenNoteAfter(notaActual: Note, nuevaNota: UpdateNoteDto) {
     const fechaNota1 = new Date(notaActual.lastModified);
     const fechaNota2 = new Date(nuevaNota.lastModified);
-
-    console.log(fechaNota1, fechaNota2, fechaNota1 < fechaNota2)
     
     if (fechaNota1 < fechaNota2) {
         return true;
